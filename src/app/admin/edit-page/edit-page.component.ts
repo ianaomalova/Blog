76def2b60a2600a575ad023d2986794608544b79
Iana <malovayai@gmail.com>
2023-08-23 23:56:15 +0300

    admin done

--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {PostsService} from "../../shared/posts.service";
-import {switchMap} from "rxjs";
+import {Subscription, switchMap} from "rxjs";
 import {Post} from "../../shared/interfaces";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 
@@ -10,8 +10,12 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
   templateUrl: './edit-page.component.html',
   styleUrls: ['./edit-page.component.scss']
 })
-export class EditPageComponent implements OnInit {
+export class EditPageComponent implements OnInit, OnDestroy {
   form: FormGroup = new FormGroup({});
+  // @ts-ignore
+  post: Post;
+  submitted = false;
+  uSub = new Subscription();
 
   constructor(private route: ActivatedRoute, private postsService: PostsService) {
 
@@ -21,6 +25,7 @@ export class EditPageComponent implements OnInit {
     this.route.params.pipe(switchMap((params) => {
       return this.postsService.getById(params['id'])
     })).subscribe((post: Post) => {
+      this.post = post;
       this.form = new FormGroup({
         title: new FormControl(post.title, Validators.required),
         text: new FormControl(post.text, Validators.required)
@@ -31,7 +36,24 @@ export class EditPageComponent implements OnInit {
   }
 
   submit() {
+    if(this.form.invalid) {
+      return;
+    }
+    this.submitted = true;
+    this.uSub = this.postsService.update({
+      ...this.post,
+      id: this.post.id,
+      text: this.form.value.text,
+      title: this.form.value.title
+    }).subscribe(() => {
+      this.submitted = false;
+    })
+  }
 
+  ngOnDestroy(): void {
+    if(this.uSub) {
+      this.uSub.unsubscribe();
+    }
   }
 }
 
